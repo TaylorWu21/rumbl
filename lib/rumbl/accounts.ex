674c0e1b16a1fcaef4911be612aa1cf4464d5dac
Taylor Wu <taylorwu21@gmail.com>
2019-05-30 19:30:53 -0600

    finish chapter 4 - Ecto and changesets

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -4,22 +4,27 @@ defmodule Rumbl.Accounts do
   """
 
   alias Rumbl.Accounts.User
+  alias Rumbl.Repo
 
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "jose"},
-      %User{id: "2", name: "Jose 2", username: "jose3"},
-      %User{id: "3", name: "Jose 3", username: "jose3"}
-    ]
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
   end
 end
