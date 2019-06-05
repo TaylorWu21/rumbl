749ece72d34926f7fb89249e970efec7800b793c
Taylor Wu <taylorwu21@gmail.com>
2019-06-04 23:04:08 -0600

    chapter 5 - authentication

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,9 +2,68 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
+  import Ecto.Query, warn: false
 
   alias Rumbl.Accounts.User
   alias Rumbl.Repo
+  alias Rumbl.Accounts.Credential
+
+  def list_credentials do
+    Repo.all(Credential)
+  end
+
+  def get_credential!(id), do: Repo.get!(Credential, id)
+
+  def create_credential(attrs \\ %{}) do
+    %Credential{}
+    |> Credential.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_credential(%Credential{} = credential, attrs) do
+    credential
+    |> Credential.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_credential(%Credential{} = credential) do
+    Repo.delete(credential)
+  end
+
+  def change_credential(%Credential{} = credential) do
+    Credential.changeset(credential, %{})
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_password(email, given_password) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_password, user.credential.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        {:error, :not_found}
+    end
+  end
 
   def change_user(%User{} = user) do
     User.changeset(user, %{})
