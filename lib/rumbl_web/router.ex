76764639610535bc0edc4f601967c45e06360b75
Taylor Wu <taylorwu21@gmail.com>
2019-07-07 16:17:30 -0600

    finish chapter 9

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -17,9 +17,10 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser # Use the default browser stack
 
+    get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
-    get "/", PageController, :index
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", RumblWeb do
